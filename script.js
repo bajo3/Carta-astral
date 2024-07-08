8e84c41012f85c5a0411929fd9f2668ee5f002bc
Felipe Lentini <99766298+bajo3@users.noreply.github.com>
2024-07-08 10:07:07 -0300

    Cambios en los colores

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-// scripts.js
-
 document.addEventListener('DOMContentLoaded', () => {
-    // You can add any JavaScript functionality here if needed.
+    const starField = document.getElementById('starfield');
+    const numberOfStars = 100; // Puedes ajustar el número de estrellas
+
+    for (let i = 0; i < numberOfStars; i++) {
+        const star = document.createElement('div');
+        star.classList.add('star');
+        star.style.top = `${Math.random() * 100}%`;
+        star.style.left = `${Math.random() * 100}%`;
+        starField.appendChild(star);
+    }
 });
